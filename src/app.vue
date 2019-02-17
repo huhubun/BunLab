93bcdf7ab0af0f49f613c2d65dfb05fbc96bb2cb
huhubun <nongzc@outlook.com>
2019-02-18 00:23:20 +0800

    添加 header 和 footer

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,31 @@
 <template>
-  <div id="app">
+  <div>
+    <lab-header/>
     <div id="url">{{ text }}</div>
+    <lab-footer/>
   </div>
 </template>
 
 <script>
+import LabHeader from './components/lab-header.vue'
+import LabFooter from './components/lab-footer.vue'
 export default {
   data() {
     return {
       text: 'lab.nzc.me'
     }
+  },
+  components: {
+    LabHeader,
+    LabFooter
   }
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
+body
+  background-color: rgb(253, 246, 227)
+
 #url
   color: red
 </style>
---
+++ src/components/lab-header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <div class="pure-menu pure-menu-horizontal">
+      <a href="/" class="pure-menu-link pure-menu-heading">Bun Web Lab
+        <small>beta</small>
+      </a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+header
+  padding: 0.6em 3em
+  background-color: rgb(238, 232, 213)
+
+header .pure-menu-heading
+  text-transform: none
+  font-size: 1.1em
+
+header .pure-menu-link
+  color: rgb(97, 97, 97)
+
+header .pure-menu-link:hover
+  background-color: transparent
+
+header a small
+  font-size: 0.6em
+  border: 1px solid rgb(114, 114, 114)
+  padding: 0.02em 0.4em
+  border-radius: 0.8em
+
+header .project-name
+  border-left: 1px solid rgb(114, 114, 114)
+</style>
