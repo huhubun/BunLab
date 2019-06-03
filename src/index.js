6ab7f8a060e7dc9cc8519c856ac1a45a97c97d51
huhubun <nongzc@outlook.com>
2019-06-04 01:34:54 +0800

    引入 vue-awesome 做图标

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './app.vue'
-import routers from './router/router' 
+import routers from './router/router'
 import api from './http'
+
+import 'vue-awesome/icons/regular/circle'
+import 'vue-awesome/icons/circle-notch'
+import Icon from 'vue-awesome/components/Icon.vue'
 
 Vue.use(VueRouter)
 Vue.use(api)
+
+Vue.component('v-icon', Icon)
 
 const router = new VueRouter({
     mode: 'history',
