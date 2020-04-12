3e7ec89dd6e9c2e8209c4cc1d441f8abaa24a73a
huhubun <nongzc@outlook.com>
2020-04-13 02:21:27 +0800

    添加 About 页面，显示程序各种信息和作者博客、源码地址

---
+++ src/BunLab/Shared/Common/LabListItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BunLab.Shared.Common
+{
+    public class LabListItem
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public string Url { get; set; }
+
+        public string UrlTitle { get; set; }
+    }
+}
---
+++ src/BunLab/Shared/Models/Informations/ServerInformationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BunLab.Shared.Models.Informations
+{
+    public class ServerInformationModel
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string RuntimeFramework { get; set; }
+
+        public string OS { get; set; }
+    }
+}
