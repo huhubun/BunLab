7173435f44bacc1eebc0b5f4891f1d2a8ae9b248
huhubun <nongzc@outlook.com>
2020-04-14 01:59:37 +0800

    现在支持复制GUIDGen生成的GUID、UA显示的结果了

--- src/BunLab/Client/wwwroot/interop.js
+++ src/BunLab/Client/wwwroot/interop.js
@@ -1,3 +1,7 @@
 ﻿window.getNavigatorProperty = function (propertyName) {
     return window.navigator[propertyName].toString()
 }
+
+window.initClipboard = function (selector){
+    new ClipboardJS(selector || '.can-copied')
+}
